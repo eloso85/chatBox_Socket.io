65d2aa85f79752f47bcf9a617dd773cc4a7408fb
Alejandro <eloso85@gmail.com>
2020-06-03 00:47:58 -0500

    3rd deployment

--- server.js
+++ server.js
@@ -70,6 +70,8 @@ io.on("connection", socket =>{
    })
 })
 
-const PORT = 3000 || process.env.PORT;
+const PORT =  process.env.PORT || 3000;
+//const PORT = 3000 || process.env.PORT;
+
 
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
